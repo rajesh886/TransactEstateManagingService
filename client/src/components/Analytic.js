2664ffac865369f2907b1a09a227b8ff73fbae52
Rhea Pottathuparambil <52354140+rheapottathuparambil@users.noreply.github.com>
2021-04-26 16:31:47 -0500

    Update Analytic.js

--- client/src/components/Analytic.js
+++ client/src/components/Analytic.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchCategory } from '../actions';
+import { fetchOnlineCount } from '../actions';
+import { fetchOfflineCount } from '../actions';
 import Chart from './Chart';
 
 class Analytic extends Component {
     componentDidMount() {
         this.props.fetchCategory();
+        this.props.fetchOfflineCount();
+        this.props.fetchOnlineCount();
     }
 
     render() {
         return (
             <div >
-
                 <Chart {...this.props} />
             </div>
         );
@@ -19,6 +22,6 @@ class Analytic extends Component {
 }
 
 function mapStateToProps(state) {
-    return { deviceCategory: state.deviceInfo.devicesCategory }
+    return { deviceCategory: state.deviceInfo.devicesCategory, deviceOfflineCount: state.deviceInfo.offlineCount, deviceOnlineCount: state.deviceInfo.onlineCount }
 }
-export default connect(mapStateToProps, { fetchCategory })(Analytic);
+export default connect(mapStateToProps, { fetchCategory, fetchOfflineCount, fetchOnlineCount })(Analytic);
